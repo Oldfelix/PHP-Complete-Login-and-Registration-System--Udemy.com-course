9da04ce8a97dc6cfec3e0c4a9817e8fa8b668de2
Oldfelix <jim.hansen@yahoo.com>
2017-02-06 17:56:49 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,13 @@
 <body>
 <h2>User Authentication System </h2><hr>
 
+<?php include_once 'resource/Database.php' ?>
 
-<?php include_once 'resource/Database.php';?>
+
+<P> Your currently not signed in <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </P>
+<p> You are logged in as {username} <a href="logout.php">Logout</a> </p>
+
+
+
 </body>
 </html>
