b1ca1ddfd35d7689b95057219e8ddfd4dcb4462a
Oldfelix <jim.hansen@yahoo.com>
2017-02-06 17:57:39 -0700

    Add files via upload

--- resource/Database.php
+++ resource/Database.php
@@ -1,5 +1,19 @@
 <?php
 
-$db = new PDO('mysql:host=localhost; dbname=register', 'root', '');
+$username = 'root';
 
-echo "Connected to the register database";
+$dsn = 'mysql:host=localhost; dbname=register';
+$password = 'root';
+
+try{
+        $db = new PDO($dsn, $username, $password);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected to the register database";
+
+}catch (PDOException $ex) {
+        echo "Connection failed ".$ex->getMessage();
+}
+?>
+
+
