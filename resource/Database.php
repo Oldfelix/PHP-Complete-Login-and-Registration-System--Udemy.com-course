2eaf79bd8a142a539b41dc57b05904ca38a8272e
Jim Hansen <kermijim@gmail.com>
2017-02-10 12:57:13 -0700

    Upadate

--- resource/Database.php
+++ resource/Database.php
@@ -1,5 +1,13 @@
 <?php
+$username = 'root';
+$dsn = 'mysql:host=localhost; dbname=register';
+$password = '';
+try{
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//              echo "Connected to the register database";
+}catch (PDOException $ex) {
+        echo "Connection failed ".$ex->getMessage();
+}
+?>
 
-$db = new PDO('mysql:host=localhost; dbname=register', 'root', '');
-
-echo "Connected to the register database";
