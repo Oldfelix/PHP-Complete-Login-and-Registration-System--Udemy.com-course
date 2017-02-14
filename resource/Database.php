46e4f9a9e5862f4ecf59769c15e6e80f8a21e3de
Oldfelix <jim.hansen@yahoo.com>
2017-02-14 16:15:13 -0700

    Add files via upload
    Update. Has Login Errors.

--- resource/Database.php
+++ resource/Database.php
@@ -1,29 +1,20 @@
-<<<<<<< HEAD
-<?php
-$username = 'root';
-$dsn = 'mysql:host=localhost; dbname=register';
-$password = '';
-try{
-        $db = new PDO($dsn, $username, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//              echo "Connected to the register database";
-}catch (PDOException $ex) {
-        echo "Connection failed ".$ex->getMessage();
-}
-?>
-
-=======
 <?php
 $username = 'root';
 $dsn = 'mysql:host=localhost; dbname=register';
 $password = 'root';
+
 try{
-        $db = new PDO($dsn, $username, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//              echo "Connected to the register database";
+	//create an instance of the PDO clas with the required parameters
+	$db = new PDO($dsn, $username, $password);
+
+	//set pdo error mode to exception
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//	display success message
+//	echo "Connected to the register database";
+
 }catch (PDOException $ex) {
-        echo "Connection failed ".$ex->getMessage();
+	//display error message
+	echo "Connection failed ".$ex->getMessage();
 }
 ?>
-
->>>>>>> origin/master
