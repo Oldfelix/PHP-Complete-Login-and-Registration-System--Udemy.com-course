1fe366595c08d82cb4ee26958fd5b3eec63d067d
Oldfelix <jim.hansen@yahoo.com>
2017-02-21 10:05:35 -0700

    Update

--- login.php
+++ login.php
@@ -5,10 +5,11 @@ include_once 'resource/utilities.php';
 
 if(isset($_POST['loginBtn'])){
 	//array to hold errors
-$form_errors = array();
+
 //validate the form
+$form_errors = array();
 $required_fields = array('username', 'password');
-$form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+$form_errors = array _merge($form_errors, check_empty_fields($required_fields));
 if(empty($form_errors)){
 	//check if user exsists in the database	
 }else{
@@ -28,15 +29,18 @@ if(empty($form_errors)){
 </head>
 <body>
 <h2>User Authentication System </h2><hr>
+<h3> Login Form</h3>
+
 <?php if(isset($result)) echo $result; ?>
 <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
-<h3> Login Form</h3>
+
 <form method="post" action="">
 	<table>
 		<tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
 		<tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
-		<tr><td></td><td><input style="float: right;" type="submit" name="loginBtn" value="Sign in"></td></tr>
+		<tr><td></td><td><input style="float: right;" type="submit" name="loginBtn" value="Signin"></td></tr>
 	</table>
+</form>
 <p> <a href="index.php">Back</a> </p>
 </body>
 </html>
