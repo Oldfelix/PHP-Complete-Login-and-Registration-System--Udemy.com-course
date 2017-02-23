949a333570f1b034f2cc5acc69b5458bbe509dd8
Oldfelix <jim.hansen@yahoo.com>
2017-02-23 16:22:16 -0700

    Update

--- contact_page.php
+++ contact_page.php
@@ -1,40 +1,41 @@
 <?php
 //add our database connection script
 include_once 'resource/Database.php';
-include_once 'resource/utilities.php';
-
+include_once 'resource/utilities2.php';
+include_once 'resource/session.php';
 //process the form
-if(isset($_POST['submitBtn'])){
+if(isset($_POST['SubmitBtn'])){
     //initialize an array to store any error message from the form
-    $form_errors2 = array();
+    $form_errors = array();
 
     //Form validation
     $required_fields = array('sender_email', 'sender_name', 'message');
 
     //call the function to check empty field and merge the return data into form_error array
-    $form_errors2 = array_merge($form_errors2, check_empty_fields($required_fields));
+    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
 
     //Fields that requires checking for minimum length
-    $fields_to_check_length = array('sender_name' => 4);
+    $fields_to_check_length = array('sender_name' => 4, 'sender_email' => 12);
 
     //call the function to check minimum required length and merge the return data into form_error array
-    $form_errors2 = array_merge($form_errors2, check_min_length($fields_to_check_length));
+    $form_errors = array_merge($form_errors, check_min_length($fields_to_check_length));
 
-    //email validation / merge the return data into form_error array
-    $form_errors2 = array_merge($form_errors2, check_sender_email($_POST));
+    //sender_email validation / merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_sender_email($_POST));
 
     //check if error array is empty, if yes process form data and insert record
-    if(empty($form_errors2)){
+    if(empty($form_errors)){
         //collect form data and store in variables
         $sender_email = $_POST['sender_email'];
         $sender_name = $_POST['sender_name'];
         $message = $_POST['message'];
 
+        
         try{
 
             //create SQL insert statement
-            $sqlInsert = "INSERT INTO feedback (sender_email, sender_name, message, send_date)
-              VALUES (:sender_email, :sender_name, :message, now())";
+            $sqlInsert = "INSERT INTO feedback (sender_name, sender_email, message, send_data)
+              VALUES (:sender_name, :sender_email, :message, now())";
 
             //use PDO prepared to sanitize data
             $statement = $db->prepare($sqlInsert);
@@ -51,10 +52,10 @@ if(isset($_POST['submitBtn'])){
         }
     }
     else{
-        if(count($form_errors2) == 1){
+        if(count($form_errors) == 1){
             $result = "<p style='color: red;'> There was 1 error in the form<br>";
         }else{
-            $result = "<p style='color: red;'> There were " .count($form_errors2). " errors in the form <br>";
+            $result = "<p style='color: red;'> There were " .count($form_errors). " errors in the form <br>";
         }
     }
 
@@ -73,13 +74,13 @@ if(isset($_POST['submitBtn'])){
 <h3>Contact Form</h3>
 
 <?php if(isset($result)) echo $result; ?>
-<?php if(!empty($form_errors2)) echo show_errors($form_errors2); ?>
+<?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
 <form method="post" action="">
     <table>
         <tr><td>Email:</td> <td><input type="text" value="" name="sender_email"></td></tr>
         <tr><td>Username:</td> <td><input type="text" value="" name="sender_name"></td></tr>
-        <tr><td>Message:</td> <td><input type="text" value="" name="message"></td></tr>
-        <tr><td></td><td><input style="float: right;" type="submit" name="submitBtn" value="Send"></td></tr>
+        <tr><td>Message:</td> <td><input   style="height:100px; width:550" type="text" value="" name="message"></td></tr>
+        <tr><td></td><td><input style="float: right;" type="submit" name="SubmitBtn" value="Submit"></td></tr>
     </table>
 </form>
 <p><a href="index.php">Back</a> </p>
--- login.php
+++ login.php
@@ -4,44 +4,68 @@ include_once 'resource/Database.php';
 include_once 'resource/utilities.php';
 
 if(isset($_POST['loginBtn'])){
-	//array to hold errors
+    //array to hold errors
+    $form_errors = array();
 
-//validate the form
-$form_errors = array();
-$required_fields = array('username', 'password');
-$form_errors = array _merge($form_errors, check_empty_fields($required_fields));
-if(empty($form_errors)){
-	//check if user exsists in the database	
-}else{
-	if(count($form_errors) == 1)){
-		$result = "<p style='color: red;'>There was one error in the form </p>";
-	}else{
-		$result = "<p style='color: red;'>There were " .count($form_errors). " errors in the form</p>";
-	}
-  }
+//validate
+    $required_fields = array('username', 'password');
+    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+
+    if(empty($form_errors)){
+
+        //collect form data
+        $user = $_POST['username'];
+        $password = $_POST['password'];
+        //check if user exist in the database
+        $sqlQuery = "SELECT * FROM users WHERE username = :username";
+        $statement = $db->prepare($sqlQuery);
+        $statement->execute(array(':username' => $user));
+
+       while($row = $statement->fetch()){
+           $id = $row['id'];
+           $hashed_password = $row['password'];
+           $username = $row['username'];
+
+           if(password_verify($password, $hashed_password)){
+               $_SESSION['id'] = $id;
+               $_SESSION['username'] = $username;
+               header("location: index.php");
+           }else{
+               $result = flashMessage("Invalid username or password");
+           }
+       }
+
+    }else{
+        if(count($form_errors) == 1){
+            $result = flashMessage("There was one error in the form");
+        }else{
+            $result = flashMessage("There were " .count($form_errors). " error in the form");
+
+			}
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Homepage</title>
+    <title>Login Page</title>
 </head>
 <body>
 <h2>User Authentication System </h2><hr>
-<h3> Login Form</h3>
+
+<h3>Login Form</h3>
 
 <?php if(isset($result)) echo $result; ?>
 <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
-
 <form method="post" action="">
-	<table>
-		<tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
-		<tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
-		<tr><td></td><td><input style="float: right;" type="submit" name="loginBtn" value="Signin"></td></tr>
-	</table>
+    <table>
+        <tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
+        <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
+        <tr><td><a href="forgot_password.php">Forgot Password?</a></td><td><input style="float: right;" type="submit" name="loginBtn" value="Signin"></td></tr>
+    </table>
 </form>
-<p> <a href="index.php">Back</a> </p>
+<p><a href="index.php">Back</a> </p>
 </body>
 </html>
-
