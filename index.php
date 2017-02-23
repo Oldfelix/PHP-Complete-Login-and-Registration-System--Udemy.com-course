4de174140462e7eb6e02c9c25f776058ed2d1ea9
Oldfelix <jim.hansen@yahoo.com>
2017-02-23 16:21:08 -0700

    Update

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php include_once 'resource/session.php' ?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -8,11 +7,14 @@
 <body>
 <h2>User Authentication System </h2><hr>
 
-<?php if(!isset($_SESSION['username'])): ?>
-<P>You are currently not signin <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signed up</a>Leave Message<a href="contact_page.php">Contact</a> </P>
-<?php else: ?>
-<p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a> </p>
-<?php endif ?>
+<?php include_once 'resource/Database.php' ?>
+
+
+<P> Your currently not signed in <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </P>
+
+<p> You are logged in as {username} <a href="logout.php">Logout</a> </p>
+
+
 
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,4 +1,1 @@
-<?php include_once 'resource/session.php';
 
-session_destroy();
-header('location: index.php');
